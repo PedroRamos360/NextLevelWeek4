b0f8e2026254d0769ed7f705c4440ed8b310e0b3
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2021-02-24 21:57:45 -0300

    fim da aula 2 e início da aula 3

---
+++ moveit-next/src/components/Countdown.tsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from 'react';
+import styles from '../styles/components/Countdown.module.css';
+
+export default function Countdown() {
+	const [time, setTime] = useState(25 * 60);
+	const [isActive, setIsActive] = useState(false);
+
+	const minutes = Math.floor(time / 60);
+	const seconds = time % 60;
+
+	// padStart preenche com '0' no início se o length não for 2
+	const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+	const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
+	function startCountDown() {
+		setIsActive(true);
+	}
+
+	useEffect(() => {
+		if (isActive && time > 0) {
+			setTimeout(() => {
+				setTime(time - 1);
+			}, 1000);
+		}
+	}, [isActive, time]);
+
+	return (
+		<div>
+			<div className={styles.countdownContainer}>
+				<div>
+					<span>{minuteLeft}</span>
+					<span>{minuteRight}</span>
+				</div>
+				<span>:</span>
+				<div>
+					<span>{secondLeft}</span>
+					<span>{secondRight}</span>
+				</div>
+			</div>
+
+			{ isActive ? (
+				<button
+					type="button"
+					className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+					onClick={startCountDown}
+				>
+					Abandonar ciclo
+				</button>
+			) : (
+				<button
+					type="button"
+					className={styles.countdownButton}
+					onClick={startCountDown}
+				>
+					Iniciar ciclo
+				</button>
+			)}
+		</div>
+	);
+}
